a6e63bfb24d1c3c2347f84d7eb601d26768998ee
Kyle Finnegan <Kyle Finnegan>
2017-04-10 22:13:15 -0500

    some work

--- main.py
+++ main.py
@@ -2,21 +2,33 @@ import webapp2
 import cgi
 import jinja2
 import os
+from google.appengine.ext import db
+
 
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
-class MainHandler(webapp2.RequestHandler):
+class BlogPost(db.Model):
+    title = db.StringProperty(required = True)
+    body = db.TextareaProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+
+class Handler(webapp2.RequestHandler):
+    def renderError(self, error_code):
+        self.error(error_code)
+        self.response.write("Oops! Something went wrong.")
+
+class Index(Handler):
     def get(self):
         t = jinja_env.get_template("frontpage.html")
         error = cgi.escape(self.request.get("error"), quote=True)
         content = t.render(error=error)
         self.response.write(content)
 
-#class Blog(webapp2.RequestHandler):
+#class Blog(Handler):
     #def post(self):
 
-class NewPost(webapp2.RequestHandler):
+class NewPost(Handler):
     def post(self):
             New_post = self.request.get("newpost")
 
@@ -27,7 +39,7 @@ class NewPost(webapp2.RequestHandler):
             self.response.write(content)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/Blog'),
-    ('/NewPost')
+    ('/', Index),
+    ('/blog', Blog),
+    ('/newpost', NewPost)
 ], debug=True)
