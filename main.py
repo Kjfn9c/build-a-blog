ebe79dde43620496f6b834ae0a88d0b1cb902558
Kyle Finnegan <Kyle Finnegan>
2017-04-18 20:46:13 -0500

    not finished, but close

--- main.py
+++ main.py
@@ -4,42 +4,66 @@ import jinja2
 import os
 from google.appengine.ext import db
 
-
 template_dir = os.path.join(os.path.dirname(__file__), "templates")
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
 
 class BlogPost(db.Model):
     title = db.StringProperty(required = True)
-    body = db.TextareaProperty(required = True)
+    body = db.StringProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
 
 class Handler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
     def renderError(self, error_code):
         self.error(error_code)
         self.response.write("Oops! Something went wrong.")
 
-class Index(Handler):
-    def get(self):
-        t = jinja_env.get_template("frontpage.html")
-        error = cgi.escape(self.request.get("error"), quote=True)
-        content = t.render(error=error)
-        self.response.write(content)
+class MainPage(Handler):
+    def render_front(self, title="", body="", error=""):
+        posts = db.GqlQuery('SELECT * FROM BlogPost ORDER BY created DESC LIMIT 5')
+        self.render("Frontpage.html", title = title, body = body, error = error, posts = posts)
 
-#class Blog(Handler):
-    #def post(self):
+    def get(self):
+        self.render_front()
+
+    def post(self):
+        title = self.request.get("title")
+        body = self.request.get("body")
+
+        if title and body:
+            a = BlogPost(title=title, body=body)
+            a.put()
+            self.redirect("/")
+        else:
+            error = "Actually type something!"
+            self.render_front(title, body, error)
 
 class NewPost(Handler):
-    def post(self):
-            New_post = self.request.get("newpost")
+        def render_newpost(self, title="", body="", error=""):
+            posts = db.GqlQuery('SELECT * FROM BlogPost ORDER BY created DESC LIMIT 5')
+            self.render("newpost.html", title = title, body = body, error = error, posts = posts)
 
-            New_post_escaped = cgi-escape(New_post, quote=True)
-            t = jinja_env.get_template("newpost.html")
-            content = t.render(newpost = new_movie_escaped)
+        def get(self):
+            self.render_newpost()
 
-            self.response.write(content)
+        def post(self):
+            title = self.request.get("title")
+            body = self.request.get("body")
+
+#class ViewPostHandler(webapp2.RequestHandler):
+    #def get(self, id):
+        #self.response.write()
+
+#webapp2.Route('//<id:\d+>', ViewPostHandler)
 
 app = webapp2.WSGIApplication([
-    ('/', Index),
-    ('/blog', Blog),
+    ('/', MainPage),
     ('/newpost', NewPost)
+    #('//<id:\d+>', ViewPostHandler)
 ], debug=True)
